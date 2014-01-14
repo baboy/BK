83be793dfd18f060cf9881d81851fcfb8de2d07b
baboy <baboyzyh@gmail.com>
2014-01-14 09:14:14 +0800

    _

--- modules/api/c/ApiConfig.php
+++ modules/api/c/ApiConfig.php
@@ -7,7 +7,7 @@ class ApiConfigHandler extends bf\core\HttpRequestHandler{
 						"position"=>"0,0,0.2286,1",
 						"title"=>"电视剧",
 						"icon"=>"",
-						"pic"=>"",
+						"pic"=>"http://app.tvie.com.cn/static/images/serial.jpg",
 						"api"=>"http://app.tvie.com.cn/api/v1/query/movie",
 						"filter"=>""
 					),
@@ -15,7 +15,7 @@ class ApiConfigHandler extends bf\core\HttpRequestHandler{
 						"position"=>"0.2286,0,0.3857,0.5",
 						"title"=>"电影",
 						"icon"=>"",
-						"pic"=>"",
+						"pic"=>"http://app.tvie.com.cn/static/images/movie.png",
 						"api"=>"http://app.tvie.com.cn/api/v1/query/movie",
 						"filter"=>""
 					),
@@ -23,7 +23,7 @@ class ApiConfigHandler extends bf\core\HttpRequestHandler{
 						"position"=>"0.6143,0,0.3857,0.5",
 						"title"=>"综艺",
 						"icon"=>"",
-						"pic"=>"",
+						"pic"=>"http://app.tvie.com.cn/static/images/zy.png",
 						"api"=>"http://app.tvie.com.cn/api/v1/query/movie",
 						"filter"=>""
 					),
@@ -31,7 +31,7 @@ class ApiConfigHandler extends bf\core\HttpRequestHandler{
 						"position"=>"0.2286,0.5,0.3,0.5",
 						"title"=>"王牌栏目",
 						"icon"=>"",
-						"pic"=>"",
+						"pic"=>"http://app.tvie.com.cn/static/images/wp.png",
 						"api"=>"http://app.tvie.com.cn/api/v1/query/movie",
 						"filter"=>""
 					),
@@ -39,7 +39,7 @@ class ApiConfigHandler extends bf\core\HttpRequestHandler{
 						"position"=>"0.5286,0.5,0.2357,0.5",
 						"title"=>"明星",
 						"icon"=>"",
-						"pic"=>"",
+						"pic"=>"http://app.tvie.com.cn/static/images/star.png",
 						"api"=>"http://app.tvie.com.cn/api/v1/query/movie",
 						"filter"=>""
 					),
@@ -47,7 +47,7 @@ class ApiConfigHandler extends bf\core\HttpRequestHandler{
 						"position"=>"0.7643,0.5,0.2357,0.5",
 						"title"=>"互动",
 						"icon"=>"",
-						"pic"=>"",
+						"pic"=>"http://app.tvie.com.cn/static/images/chat.png",
 						"api"=>"http://app.tvie.com.cn/api/v1/query/movie",
 						"filter"=>""
 					)
