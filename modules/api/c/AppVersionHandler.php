c4d8ecea5f17b0e7b9586704daf42b0b5bc28a67
baboy <baboyzyh@gmail.com>
2014-01-16 18:53:57 +0800

    _

---
+++ modules/api/c/AppVersionHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+class AppVersionHandler extends bf\core\HttpRequestHandler{
+
+	function getModel($modelName){
+		if (empty($this->model)) {
+			require_once dirname(__FILE__)."/../m/$modelName.php";
+			$this->model = new $modelName();
+		}
+		return $this->model;
+	}
+	function init(){
+		$this->getModel("AppVersion");
+	}
+	function updateParam(){
+		$fields = array(
+				"package"=>array("type"=>"string"),
+				"version"=>array("type"=>"float"),
+				"build"=>array("type"=>"int"),
+				"mac"=>array("type"=>"string","option"=>true),
+				"channel"=>array("type"=>"string","option"=>true),
+				"device_id"=>array("type"=>"string","option"=>true)
+			);
+		return $fields;
+	}
+	function update($param){
+		$app = $this->model->queryLastVersion($param["package"], empty($param["channel"]) ? null : $param["channel"],"publish");
+		$status = bf\core\Status::status();
+		$data = array("role"=>0, "msg"=>"没有可更新的版本!", "version"=>$param["version"], "build"=>$param["build"]);
+		if (!empty($app) && $app->version > $param["version"]) {
+			$data["role"] = 10;
+			$data["download_url"] = $app->download_url;
+			$data["msg"] = "有新版本更新！";
+			$data["version"] = $app->version;
+			$data["build"] = $app->build;
+		}
+		$status->data = $data;
+		return $status;
+	}
+}
---
+++ modules/api/m/AppVersion.php
@@ -0,0 +1,19 @@
+<?php
+
+class AppVersion extends bf\core\Model{
+	function queryLastVersion($package, $channel, $status){
+		$where = "app.package='$package'";
+		if (!empty($status))
+			$where .= " AND build.status='$status'";
+		if ($channel) {
+			$where .= " AND build.channel='$channel'";
+		}else{
+			$where .= " AND build.channel is NULL";
+		}
+		$sql = "select app.*,build.build,build.version,build.download_url "
+			. "from wp_app app,wp_app_build build "
+			. "where $where and build.appid=app.id order by build.version desc, build.build desc limit 0,1";
+		$app = $this->db->query($sql);
+		return $app?$app[0]:null;
+	}
+}
